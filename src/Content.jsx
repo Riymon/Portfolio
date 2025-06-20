65f0e934af4d03c072f76e8dae08dd00befebb96
riy <riymonarong@gmail.com>
2025-06-21 07:13:33 +0800

    modified gradient home page

--- src/Content.jsx
+++ src/Content.jsx
@@ -3,17 +3,17 @@ function Content() {
     <>
     <h1 className="text2 pr-250 text-shadow-2xs text-4xl">My Services</h1>
     <div className="text1 grid grid-cols-3 gap-4 h-[50vh] my-5 p-4">
-        <div className="content">
+        <div className="content bg-gradient-to-b from-[#000000] to-[#8f091f]">
             <h1 className="text-2xl ">HTML & CSS</h1>
             <p>Another short description can go here.</p>
         </div>
       
-      <div className="content">
+      <div className="content bg-gradient-to-b from-[#000000] to-[#8f091f]">
         <h2 className="text-2xl">2nd head</h2>
         <p>Another short description can go here.</p>
       </div>
 
-      <div className="content">
+      <div className="content bg-gradient-to-b from-[#000000] to-[#8f091f]">
         <h2 className="text-2xl">3rd head</h2>
         <p>This fills out the third column with more info.</p>
       </div>
--- src/body.jsx
+++ src/body.jsx
@@ -2,12 +2,12 @@ import test from './images/test.png';
 function Body(){
 
     return(
-        <section className="relative w-100% h-50% bg-gradient-to-br from-[#181818] to-[#8f091f] overflow-hidden">
-    {/* Curved Background Shape */}
-    <div className="absolute -top-10 left-0 w-full h-1/2 bg-[#0c0c0c] rounded-b-[50%] blur-sm opacity-90"></div>
+        <section className="relative w-full h-screen bg-gradient-to-b from-[#000000] to-[#8f091f] overflow-hidden">
+            {/* Background Curved Shape */}
+            <div className="absolute bottom-0 w-full h-[30vh] bg-gradient-to-t from-[#000000] rounded-t-[0%] opacity-90"></div>
 
-    {/* Accent Shape */}
-    <div className="absolute top-1/3 right-0 w-1/3 h-1/3 bg-[#1c1b1b] rounded-tl-[100%] blur-md opacity-70 rotate-15"></div>
+            {/* Foreground Content */}
+        <div className="relative z-10 text-white px-8 pt-24"> </div>
             <div className="background relative w-full h-150 bg-black-500 overflow-hidden">
                 <div className="car-picture">
                     <img src={test} alt="my picture" class="back-img"/>
